ef75f3a1488215c4b24de3a52d8b6132885551c8
Nathan Gong <nathangong@Nathans-MacBook-Air-2.local>
2026-07-07 22:16:58 -0700

    Adding raw ohtani and trout data

---
+++ notebooks/test.py
@@ -0,0 +1,22 @@
+import pybaseball
+import pandas as pd
+
+ohtani = pybaseball.playerid_lookup('ohtani', 'shohei')
+ohtani_id = ohtani['key_mlbam'].iloc[0]
+
+trout = pybaseball.playerid_lookup('trout', 'mike')
+trout_id = trout['key_mlbam'].iloc[0]
+
+ohtani_stats = pybaseball.statcast_pitcher('2020-03-20', '2023-03-20', ohtani_id)
+trout_stats = pybaseball.statcast_batter('2020-03-20', '2023-03-20', trout_id)
+
+ohtani_stats = ohtani_stats[ohtani_stats['game_type'].isin(['R', 'F', 'D', 'L', 'W'])]
+trout_stats = trout_stats[trout_stats['game_type'].isin(['R', 'F', 'D', 'L', 'W'])]
+
+ohtani_stats.to_csv('../data/raw/ohtani_20-23.csv', index=False)
+trout_stats.to_csv('../data/raw/trout_20-23.csv', index=False)
+
+print(ohtani_stats['pitch_type'].value_counts())
+print(ohtani_stats['delta_run_exp'].isnull().sum())
+print(ohtani_stats['zone'].unique())
+print(pd.crosstab(ohtani_stats['pitch_type'], [ohtani_stats['balls'], ohtani_stats['strikes']]))
